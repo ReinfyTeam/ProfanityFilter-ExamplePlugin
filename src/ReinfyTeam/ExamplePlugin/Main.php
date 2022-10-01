01028b1b6138eb7048a68483091669221a57fc19
Ace Sobremont <xqwtxon@hotmail.com>
2022-10-01 17:26:31 +0800

    Class Update

--- src/ReinfyTeam/ExamplePlugin/Main.php
+++ src/ReinfyTeam/ExamplePlugin/Main.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerChatEvent;
-use ReinfyTeam\libpf\ProfanityFilter;
+use ReinfyTeam\libpf\libpf;
 
 class Main extends PluginBase implements Listener{
     
@@ -16,11 +16,11 @@ class Main extends PluginBase implements Listener{
     }
     
     public function onChat(PlayerChatEvent $event) : void{
-        if(ProfanityFilter::detectProfanity($event->getMessage(), ProfanityFilter::defaultProfanity())){
+        if(libpf::detectProfanity($event->getMessage(), libpf::defaultProfanity())){
             $event->cancel(); // cancel the event
             $event->getPlayer()->sendMessage(TextFormat::RED . "Please watch your language!");
         } else {
-            $event->setMessage(ProfanityFilter::removeUnicode(ProfanityFilter::removeProfanity($event->getMessage()), true)); // remove profanity + remove unicodes (bypasses)
+            $event->setMessage(libpf::removeUnicode(libpf::removeProfanity($event->getMessage()), true)); // remove profanity + remove unicodes (bypasses)
         }
     }
 }
